177a64b6df84bf4cddbd63ee9f1972cfcaafedfb
Humberto Arana <humaranah@outlook.com>
2026-01-16 11:12:42 -0500

    Switch to Swashbuckle for Swagger/OpenAPI support

--- src/DemoActions/Program.cs
+++ src/DemoActions/Program.cs
@@ -1,17 +1,30 @@
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Weather Forecast",
+        Version = "v1"
+    });
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather Forecast v1");
+    });
 }
 
 app.UseHttpsRedirection();
